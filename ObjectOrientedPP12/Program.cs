2db2a191043da99e52aefda0ce2e18151af3616e
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-27 20:19:03 +0530

    [AkankshaAnil] search for total number of unique elements in an array

--- ObjectOrientedPP12/Program.cs
+++ ObjectOrientedPP12/Program.cs
@@ -2,6 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello User!");
+        int[] items = { 2, 3, 5, 3, 7, 5 };
+        int n = items.Length;
+
+        Console.WriteLine("Unique array elements: ");
+
+        for (int i = 0; i < n; i++)
+        {
+            bool isDuplicate = false;
+            for (int j = 0; j < i; j++)
+            {
+                if (items[i] == items[j])
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate)
+            {
+                Console.WriteLine(items[i]);
+
+            }
+        }
+             Console.ReadLine();
     }
-}
+
+ }
+
+
